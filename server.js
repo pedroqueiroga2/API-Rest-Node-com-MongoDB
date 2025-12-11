420323e220f3931c30d8ff7171c5ef822418d43e
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-11 19:36:04 -0300

    criando servidor e passando a porta

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 import http from "http";
+const PORT = 3000;
+const server = http.createServer((req, res) =>
+    {
+        res.writeHead(200, {"Content-Type": "text/plan"});
+        res.end("Curso de Node.js");
+    }
+);
 
+server.listen(PORT, () =>{
+    console.log("servidor escutando!");
+
+});
