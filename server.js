9d1ed530e371b3a634279c81585170f9c3d971a6
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-11 19:49:48 -0300

    brincando com as rotas

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import http from "http";
 const PORT = 3000;
+const rotas = 
+{
+    "/node": "Curso de node.js",
+    "/Livros": "achou a livraria",
+    "/Atores": "Que isso, é Hollywood?"
+}
 const server = http.createServer((req, res) =>
     {
         res.writeHead(200, {"Content-Type": "text/plan"});
-        res.end("Curso de Node.js");
+        res.end(rotas[req.url]);
     }
 );
 
