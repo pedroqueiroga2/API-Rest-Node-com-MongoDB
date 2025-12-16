83ef40d2f6a887cfc94ca0edceb380d4decea029
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-16 20:31:44 -0300

    criando metodo pra buscar por Id e atualizar dados

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json()); //middleware, que converte as requisições enviadas como String em json para o servidor
 const livros = [
     {
         id: 1,
@@ -11,6 +12,15 @@ const livros = [
         Nome: "O pequeno príncipe"
     }
 ]
+function buscaLivro(id)
+{
+   return livros.findIndex(livro=>
+    {
+        return livro.id === Number(id);
+    });
+}
+
+
 app.get("/", (req, res) =>{
 res.status(200).send("curso de node.js");
 });
@@ -21,4 +31,26 @@ app.get("/livros", (req, res) =>
         
     });
 
+app.post("/livros", (req, res) =>
+    {
+        livros.push(req.body);
+        res.status(201).send("livro enviado");
+    });
+
+app.delete("/livros", (req, res) =>
+    {
+        livros.pop();
+        res.status(200).send("boa, deletou");
+    });
+app.get("/livros/:id", (req, res) =>
+    {
+        const index = buscaLivro(req.params.id);
+        res.status(200).json(livros[index]);
+    });
+app.put("/livros/:id", (req, res) =>
+    {
+        const index = buscaLivro(req.params.id);
+        livros[index].Nome =req.body.Nome;
+        res.status(200).json(livros);
+    });
 export default app;
