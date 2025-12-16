ef747e90931700eee71a90d07f5d375c62bfe495
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-16 19:39:18 -0300

    criando array de livros e metodo pra retornar esse array

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import express from "express";
 
 const app = express();
-
+const livros = [
+    {
+        id: 1,
+        Nome: "Código Limpo"
+    },
+    {
+        id: 2,
+        Nome: "O pequeno príncipe"
+    }
+]
 app.get("/", (req, res) =>{
 res.status(200).send("curso de node.js");
 });
+
+app.get("/livros", (req, res) =>
+    {
+        res.status(200).json(livros);
+        
+    });
 
 export default app;
