43a8e34ae0028988fd1cd50973fa1e23456443ba
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-16 19:25:42 -0300

    mudando da biblioteca http pra biblioteca Express(app.js)

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-import http from "http";
+//import http from "http";
+import app from "./src/app.js";
 const PORT = 3000;
-const rotas = 
-{
-    "/node": "Curso de node.js",
-    "/Livros": "achou a livraria",
-    "/Atores": "Que isso, é Hollywood?"
-}
-const server = http.createServer((req, res) =>
-    {
-        res.writeHead(200, {"Content-Type": "text/plan"});
-        res.end(rotas[req.url]);
-    }
-);
 
-server.listen(PORT, () =>{
+
+app.listen(PORT, () =>{
     console.log("servidor escutando!");
 
 });
