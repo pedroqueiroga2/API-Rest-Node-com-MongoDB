aead80420fac4c84bd3eae3e9951f657596f6ea8
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-18 19:55:45 -0300

    importando mongoose e criando function de conexão

---
+++ src/config/dbconnect.js
@@ -0,0 +1,14 @@
+import mongoose, {mongo} from "mongoose";
+
+async function conectaNaDatabase() 
+{
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
+    return mongoose.connection;
+};
+
+export default conectaNaDatabase;
+
+
+
+
+
