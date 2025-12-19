3593898df4899ba5365374bc025a7301710319e7
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-19 20:13:08 -0300

    adicionando rotas

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,5 +4,8 @@ import livroController from "../controllers/livroController.js";
 const routes = express.Router(); // metodo express que lida com rotas
 
 routes.get("/livros", livroController.listarLivros);
-
+routes.get("/livros/:id", livroController.listarLivroPorId);
+routes.post("/livros", livroController.cadastrarLivro);
+routes.put("/livros/:id", livroController.atualizarLivro);
+routes.delete("/livros/:id", livroController.deletarLivro);
 export default routes;
