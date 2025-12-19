ffc54a47399e7bc2c81f1618a9551aaca08954b4
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-19 20:12:34 -0300

    criando metodo put, delete e listarporId

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -5,10 +5,17 @@ class LivroController {
         const listalivros = await livro.find({});
         res.status(200).json(listalivros);
     }
+
+     static async listarLivroPorId(req, res) {
+        const id = req.params.id;
+        const listaLivro = await livro.findById(id);
+        res.status(200).json(listaLivro);
+    }
+
     static async cadastrarLivro(req, res)
     {
         try{
-            const novolivre = await livro.create(req.body);
+            const novolivro = await livro.create(req.body);
             res.status(201).json({ message: "Criado com sucesso", livro: novolivro });
         }
         catch(erro)
@@ -16,6 +23,18 @@ class LivroController {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` })
         }
         
+    }
+
+     static async atualizarLivro(req, res) {
+        const id = req.params.id;
+        await livro.findByIdAndUpdate(id, req.body);
+        res.status(200).json({message:"livro atualizado"});
+    }
+
+    static async deletarLivro(req, res) {
+        const id = req.params.id;
+        await livro.findByIdAndDelete(id);
+        res.status(200).json({message:"livro deletado"});
     }
 };
 
