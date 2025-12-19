7514e82146d70b21f1d029019c2f7b9128f51bff
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-19 20:13:31 -0300

    removendo metodos que foram usados com array

--- src/app.js
+++ src/app.js
@@ -13,28 +13,5 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-
-
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("livro enviado");
-});
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index]);
-});
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].Nome = req.body.Nome;
-    res.status(200).json(livros);
-});
-
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("deletado com sucesso");
-});
 export default app;
 
