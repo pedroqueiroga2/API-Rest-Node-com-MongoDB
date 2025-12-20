5d82031b2e99236fbe2bcc789f328c2051b877e2
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-19 21:21:20 -0300

    passando autorRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import livros from "../controllers/livroController.js"
 import livrosRoutes from "./livrosRoutes.js";
+import autorRoutes from "./autorRoutes.js"
 const routes = (app) => {
     app.route("/").get((req,res) => res.status(200).send("Curso de Node.js"));
     app.use(express.json()); //middleware, que converte as requisições enviadas como String em json para o servidor
-    app.use(livrosRoutes);
+    app.use(livrosRoutes, autorRoutes);
 };
 
 export default routes;
