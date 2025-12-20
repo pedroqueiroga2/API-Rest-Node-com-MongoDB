e95a7d55afc773b3c37bc4ca0ffb439b978f2d4c
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-19 21:31:30 -0300

    relacionando autor com livros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,5 +1,5 @@
 import livro from "../models/Livro.js";
-
+import {autor} from "../models/Autor.js"
 class LivroController {
     static async listarLivros(req, res) {
         const listalivros = await livro.find({});
@@ -14,8 +14,11 @@ class LivroController {
 
     static async cadastrarLivro(req, res)
     {
+        const novolivro = req.body;
         try{
-            const novolivro = await livro.create(req.body);
+            const autorEncontrado = await autor.findById(novolivro.autor);
+            const livroCompleto = {...novolivro, autor: {...autorEncontrado._doc}}
+            const livroCriado = await livro.create(livroCompleto);
             res.status(201).json({ message: "Criado com sucesso", livro: novolivro });
         }
         catch(erro)
