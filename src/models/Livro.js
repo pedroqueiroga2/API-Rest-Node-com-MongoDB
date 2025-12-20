4d51cc31de54215d256dafe73a8f0788d765f366
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-19 21:31:42 -0300

    fix

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import {autorSchema} from "./Autor.j"
+import {autorSchema} from "./Autor.js"
 
 const livroSchema = new mongoose.Schema({
     id: {type: mongoose.Schema.Types.ObjectId },
