041de6f813b697016e8db9cbf0d1274411e9db9a
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-24 14:06:43 -0300

    resetando proejto

--- src/routes/autorRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import autorController from "../controllers/autorController.js";
-
-const routes = express.Router();
-
-routes.post("/Autor", autorController.criarAutor); 
-routes.get("/Autor", autorController.listarAutor);
-routes.get("/Autor/:id", autorController.listarAutorporId);
-export default routes;
