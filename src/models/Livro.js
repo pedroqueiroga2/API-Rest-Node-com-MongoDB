f872080e8f545795c29d19253aa34fb30be9b544
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-24 16:04:24 -0300

    adicionando mensagem de erro personalizadas

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 const livroSchema = new mongoose.Schema(
   {
     id: {type: String},
-    titulo: {type: String, required: true},
-    autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
-    editora: {type: String, required: true},
+    titulo: {type: String, required: [true, "O Titulo é obrigatório"]},
+    autor: {type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: [true, "O nome é obrigatório"]},
+    editora: {type: String, required: [true, "A Editora é obrigatório"]},
     numeroPaginas: {type: Number}
   }
 );
