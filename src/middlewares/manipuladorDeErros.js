75af9a639eba27414ec8d10b2f7d99ddb7762a9b
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-24 15:54:59 -0300

    adicionando erro de validação de dados

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -5,6 +5,11 @@ function manipuladorDeErros(erro, req, res, next) {//middleware de erro
     if (erro instanceof mongoose.Error.CastError) {
         res.status(400).send({ message: "um ou mais parâmetros foram informados de forma inválida" });
     }
+    else if(erro instanceof mongoose.Error.ValidationError)
+        {
+            
+            res.status(400).send({message: "Houve um erro de validação de dados"})
+        }
     else {
         res.status(500).send({ message: "erro interno de servidor." });
     }
