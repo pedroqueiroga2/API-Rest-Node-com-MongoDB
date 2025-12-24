69bcd28b988cfca6e04fd1df7d92745265748f70
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-24 16:39:24 -0300

    aplicando POO

---
+++ src/erros/RequisicaoIncorreta.js
@@ -0,0 +1,12 @@
+import ErroBase from "./erroBase.js"
+
+class RequisicaoIncorreta extends ErroBase
+{
+    constructor(mensagem = "um ou mais dados estão incorretos")
+    {
+        super(mensagem, 400);
+    }
+
+}
+
+export default RequisicaoIncorreta;
