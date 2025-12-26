198946924e2b9d601773f346ee813ed6e58f43d3
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-26 15:39:26 -0300

    dinamizando busca com regex com operadores do próprio MongoDB

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -97,7 +97,7 @@ class LivroController {
       const regex = new RegExp(titulo, 'i'); //usando Regex com JS puro
 
       const busca= {};
-      if(editora) busca.editora = editora;
+      if(editora) busca.editora = {$regex: editora, $options: "i"};
       if(titulo) busca.titulo = regex; //usando Regex com JS puro
       const livrosResultado = await livros.find(busca);
       if (livrosResultado !== null) {
