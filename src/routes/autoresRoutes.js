2e6ee26640552ad15a99119a14d5c86a8c09aca3
Pedro Queiroga <pedroqueirogahub@gmail.com>
2025-12-26 15:29:04 -0300

    criando rota para novo metodo

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router
   .get('/Autor', AutorController.listarAutores)
+  .get('/Autor/busca', AutorController.listarAutoresPorFiltro)
   .get('/Autor/:id', AutorController.listarAutorPorId)
   .post('/Autor', AutorController.cadastrarAutor)
   .put('/Autor/:id', AutorController.atualizarAutor)
